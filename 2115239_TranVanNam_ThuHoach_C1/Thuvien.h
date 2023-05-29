5dcb8138d0d3fc2752d45228c69eada380d221e5
Tran Van Nam <105662653+tvnam0605@users.noreply.github.com>
2023-05-29 20:42:32 +0700

    Add files via upload

---
+++ 2115239_TranVanNam_ThuHoach_C1/Thuvien.h
@@ -0,0 +1,8 @@
+struct NhanVien
+{
+	char maNV[7];
+	char hoVaTen[15];
+	char diaChi[15];
+	int trinhDoVh;
+	double Luong;
+};
---
+++ 2115239_TranVanNam_ThuHoach_C1/program.cpp
@@ -0,0 +1,11 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+#include "Thuvien.h"
+void ChayChuongTrinh();
+int main()
+{
+	ChayChuongTrinh();
+	return 1;
+}
